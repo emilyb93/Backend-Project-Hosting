b22e37e95776f68ff43b5999a6202b65f00d05a1
emilyb93 <emilybennett93@hotmail.com>
2021-10-18 11:30:51 +0100

    installing CORS

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const apiRouter = require("./routers/api.router");
 const { send404, handlePSQLError } = require("./controllers/error.controller");
 const app = express();
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json());
 app.use("/api", apiRouter);
 
